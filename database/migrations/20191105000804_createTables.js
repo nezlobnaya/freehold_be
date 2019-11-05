dacca50c6efab9a0db0309d32c3f2255acfaf97e
Sandy <sandy.jernigan@gmail.com>
2019-11-05 01:34:01 -0500

    Removed SQL examples from knex file.

--- database/migrations/20191105000804_createTables.js
+++ database/migrations/20191105000804_createTables.js
@@ -12,21 +12,6 @@ exports.up = function(knex) {
     tbl.json("address").notNullable();
     tbl.string('type').notNullable();
     tbl.string('phone').notNullable();
-    /* SQL
-      CREATE TABLE "users" (
-        "id" serial NOT NULL UNIQUE,
-        "username" varchar(255) NOT NULL UNIQUE,
-        "password" varchar(255) NOT NULL,
-        "name" json NOT NULL,
-        "email" varchar(255) NOT NULL,
-        "address" json NOT NULL,
-        "type" char NOT NULL,
-        "phone" varchar(12) NOT NULL,
-        CONSTRAINT "users_pk" PRIMARY KEY ("id")
-      ) WITH (
-        OIDS=FALSE
-      );
-    */
   })
   
   // properties
@@ -40,21 +25,6 @@ exports.up = function(knex) {
     tbl.date('property-enddate');
     tbl.integer('landlord-id').unsigned()
       .references('id').inTable('users');
-    /* SQL
-      CREATE TABLE "properties" (
-        "id" serial NOT NULL,
-        "property-name" varchar(255) NOT NULL,
-        "property-address" json NOT NULL,
-        "property-image" varchar(255),
-        "property-status" varchar(255),
-        "property-startdate" DATE,
-        "property-enddate" DATE,
-        "landlord-id" bigint NOT NULL,
-        CONSTRAINT "properties_pk" PRIMARY KEY ("id")
-      ) WITH (
-        OIDS=FALSE
-      );
-    */
   })
   
   // tenanthistory
@@ -66,18 +36,6 @@ exports.up = function(knex) {
         .references('id').inTable('properties');
     tbl.date('history-startdate');
     tbl.date('history-enddate');
-    /* SQL
-      CREATE TABLE "tenanthistory" (
-        "id" serial NOT NULL,
-        "tenant-id" bigint NOT NULL,
-        "property-id" bigint NOT NULL,
-        "history-startdate" DATE,
-        "history-enddate" DATE,
-        CONSTRAINT "tenanthistory_pk" PRIMARY KEY ("id")
-      ) WITH (
-        OIDS=FALSE
-      );
-    */
   })
   
   // workorders
@@ -90,20 +48,6 @@ exports.up = function(knex) {
     tbl.date("wo-enddate");
     tbl.integer('property-id').unsigned()
       .references('id').inTable('properties');
-    /* SQL
-      CREATE TABLE "workorders" (
-        "id" serial NOT NULL,
-        "workorder" char NOT NULL,
-        "wo-description" TEXT NOT NULL,
-        "wo-type" varchar(255) NOT NULL,
-        "wo-startdate" DATE NOT NULL,
-        "wo-enddate" DATE,
-        "property-id" bigint NOT NULL,
-        CONSTRAINT "workorders_pk" PRIMARY KEY ("id")
-      ) WITH (
-        OIDS=FALSE
-      );
-    */
   })
   
   // wohistory
@@ -116,28 +60,14 @@ exports.up = function(knex) {
     tbl.string('wo-update');
     tbl.timestamp('wo-updatedate');
     tbl.string('wo-assignedto');
-    /* SQL
-      CREATE TABLE "wohistory" (
-        "id" serial NOT NULL,
-        "wo-id" bigint NOT NULL,
-        "wo-status" varchar(255),
-        "wo-urgency" int,
-        "wo-update" TEXT,
-        "wo-updatedate" TIMESTAMP NOT NULL,
-        "wo-assignedto" varchar(255),
-        CONSTRAINT "wohistory_pk" PRIMARY KEY ("id")
-      ) WITH (
-        OIDS=FALSE
-      );
-    */
   });
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('wohistory');
-    .dropTableIfExists('workorders');
-    .dropTableIfExists('tenanthistory');
-    .dropTableIfExists('properties');
+    .dropTableIfExists('wohistory')
+    .dropTableIfExists('workorders')
+    .dropTableIfExists('tenanthistory')
+    .dropTableIfExists('properties')
     .dropTableIfExists('users');
 };
