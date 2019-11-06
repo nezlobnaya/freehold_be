5d12483b6a20d5d70cbaf29f4b4c6cb1c9b45ed2
Sandy <sandy.jernigan@gmail.com>
2019-11-05 19:01:52 -0500

    Update server.js
    Add Basic Route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// Base Route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+// Routes
 app.get("/hello", (_req, res) => {
   res.send("Hello, world");
 });
