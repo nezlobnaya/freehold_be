67132b842bb7b513c71f3fb44e1bcf912fbb6684
Matt Hagner <web@matthagner.com>
2019-11-11 18:44:49 -0600

    Add auth router to server
    * Add post handler for `/api/auth/register` which allows a user to
      register with a valid email and password and sends back a token
    * Add post handler for `/api/auth/login` which allows a user to login
      with valid email and password and sends back a token

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const authRouter = require("./routes/auth/");
+const bearerAuth = require("./lib/bearer-auth");
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
 app.get("/hello", (_req, res) => {
   res.send("Hello, world");
+});
+
+app.get("/protected", bearerAuth, (_req, res) => {
+  res.send("Yay");
 });
 
 module.exports = app;
