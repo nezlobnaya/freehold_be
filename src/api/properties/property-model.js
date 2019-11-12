a1423084bbff936b604ea2bae6b1f43056091d59
Sandy <sandy.jernigan@gmail.com>
2019-11-11 19:47:29 -0500

    Create property-model.js

---
+++ src/api/properties/property-model.js
@@ -0,0 +1,41 @@
+const db = require('../../../database/db-config.js');
+
+module.exports = {
+  // Create
+  addProperty,
+  // Read
+  getProperty,
+  getAllProperties,
+  // Update
+  updateProperty,
+  // Delete
+  deleteProperty
+};
+
+//#region - CREATE
+
+function addProperty(input) {}
+
+//#endregion
+
+//#region - READ 
+
+// getProperty() - return results for a property by id
+function getProperty(id) {}
+
+// getAllProperties() - return all properties
+function getAllProperties() {}
+
+//#endregion - Get
+
+//#region - Update
+
+function updateProperty(changes, id) {}
+
+//#endregion
+
+//#region - Delete
+
+function deleteProperty(id) {}
+
+//#endregion
