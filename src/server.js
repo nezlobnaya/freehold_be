5c93a763a6ba795a1eea2ba3a40ebb8d345557d4
Sandy <sandy.jernigan@gmail.com>
2019-11-12 17:52:27 -0500

    Add cors and helmet

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require('cors');
+const helmet = require('helmet');
 const path = require('path');
+const app = express();
 
 // require router files
 // const authRouter = require("./routes/auth/");
 const propertyRouter = require('./routes/properties/property-router.js');
 
-const app = express();
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 // Base Route
