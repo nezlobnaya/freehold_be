f6bdd0f0a4c01a28f3bcc59d7fc4b8617b0184fe
Sandy <sandy.jernigan@gmail.com>
2019-11-12 20:28:02 -0500

    update names to camelCase

--- database/seeds/05-wohistory.js
+++ database/seeds/05-wohistory.js
@@ -3,34 +3,34 @@ exports.seed = function(knex) {
   return knex.from("wohistory").insert([
     {
       // id: 1, (auto)
-      'wo-id': 1,
-      'wo-status': 'new', // new request
-      'wo-urgency': 4, // scale of 1 to 5:
+      'woId': 1,
+      'woStatus': 'new', // new request
+      'woUrgency': 4, // scale of 1 to 5:
         // 1 - not urgent, 2 - least urgent, 3 - moderately urgent, 4 - urgent, 5 - most urgent
-      // 'wo-updatedate': timestamp
+      // 'woUpdatedate': timestamp
     },
     {
       // id: 2, (auto)
-      'wo-id': 1,
-      'wo-status': 'acknowledged', // landlord acknowledged the request
-      'wo-urgency': 4
+      'woId': 1,
+      'woStatus': 'acknowledged', // landlord acknowledged the request
+      'woUrgency': 4
     },
     {
       // id: 3, (auto)
-      'wo-id': 1,
-      'wo-status': 'assigned', // landlord has contacted and assigned work
-      'wo-urgency': 3,
-      'wo-update': {
+      'woId': 1,
+      'woStatus': 'assigned', // landlord has contacted and assigned work
+      'woUrgency': 3,
+      'woUpdate': {
         assignedTo: "ABC Plumbing",
         comment: "Waiting on confirmation on date and time frame for repair." // comment optional
       }
     },
     {
       // id: 4, (auto)
-      'wo-id': 1,
-      'wo-status': 'sending', // sending crew to repair
-      'wo-urgency': 3,
-      'wo-update': {
+      'woId': 1,
+      'woStatus': 'sending', // sending crew to repair
+      'woUrgency': 3,
+      'woUpdate': {
         assignedTo: "ABC Plumbing",
         expectedRepairDate: "11-04-2018",
         comment: "ABC Plumbing will send repair crew to site on 11-04-2018 between 12pm-2pm."
@@ -38,40 +38,40 @@ exports.seed = function(knex) {
     },
     {
       // id: 5, (auto)
-      'wo-id': 1,
-      'wo-status': 'review', // require tenant to acknowledge if repaired
-      'wo-urgency': 1,
-      'wo-update': {
+      'woId': 1,
+      'woStatus': 'review', // require tenant to acknowledge if repaired
+      'woUrgency': 1,
+      'woUpdate': {
         assignedTo: "ABC Plumbing",
         expectedRepairDate: "11-04-2018"
       }
     },
     {
       // id: 6, (auto)
-      'wo-id': 1,
-      'wo-status': 'closed', // tenant acknowledged repair
-      'wo-update': {
+      'woId': 1,
+      'woStatus': 'closed', // tenant acknowledged repair
+      'woUpdate': {
         comment: "Thank you, the issue is resolved."
       }
     },
     {
       // id: 7, (auto)
-      'wo-id': 2,
-      'wo-status': 'new', 
-      'wo-urgency': 4, 
+      'woId': 2,
+      'woStatus': 'new', 
+      'woUrgency': 4, 
     },
     {
       // id: 8, (auto)
-      'wo-id': 2,
-      'wo-status': 'acknowledged', 
-      'wo-urgency': 4, 
+      'woId': 2,
+      'woStatus': 'acknowledged', 
+      'woUrgency': 4, 
     },
     {
       // id: 9, (auto)
-      'wo-id': 2,
-      'wo-status': 'assigned', // landlord has contacted and assigned work
-      'wo-urgency': 2,
-      'wo-update': {
+      'woId': 2,
+      'woStatus': 'assigned', // landlord has contacted and assigned work
+      'woUrgency': 2,
+      'woUpdate': {
         assignedTo: "Bee Electricans",
         comment: "Waiting on confirmation on date and time frame for repair." // comment optional
       }
