fbf7fc37448e8db37249dee11588a835e54e7b09
Sandy <sandy.jernigan@gmail.com>
2019-11-17 21:52:03 -0500

    Update property-router.js

--- src/routes/properties/property-router.js
+++ src/routes/properties/property-router.js
@@ -29,16 +29,6 @@ router.get('/:id', async (req, res) => {
 });
 
 // GET all properties for a specific user
-router.get('/', async (req, res) => {
-  try {
-    const results = await Properties.getAllProperties();
-    res.json(results);
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to get results.' });
-  }
-});
-
-// GET all properties for a specific user
 router.get('/user/:email', async (req, res) => {
   const { email } = req.params;
 
