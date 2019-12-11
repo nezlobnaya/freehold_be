f7da14ce62179279088cbcb5aeee6e7044ebfbd8
Matt Hagner <web@matthagner.com>
2019-12-10 19:18:59 -0600

    Update and test middlware and lib functions

---
+++ src/lib/db-helpers.js
@@ -0,0 +1,54 @@
+const faker = require("faker");
+
+const creatUser = type => ({
+  email: faker.internet.email(),
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  street: faker.address.streetAddress(),
+  city: faker.address.city(),
+  state: faker.address.state(),
+  zip: faker.address.zipCode(),
+  phone: faker.phone.phoneNumber(),
+  type
+});
+
+const createSeedResources = (creationFn, num = 20) => {
+  let resources = [];
+
+  if (num > 0) {
+    for (let i = 0; i < num; i++) {
+      resources.push(creationFn());
+    }
+  }
+
+  return resources;
+};
+
+const createLandlord = () => creatUser("landlord");
+const createTenant = () => creatUser("tenant");
+
+const landlords = createSeedResources(createLandlord);
+const tenants = createSeedResources(createTenant);
+
+const seedUsers = [...landlords, ...tenants];
+
+const getRandomLandlordId = () => {
+  return landlords[Math.floor(Math.random() * landlords.length) - 1];
+};
+
+const createProperty = (landlordId = getRandomLandlordId()) => ({
+  name: faker.company.companyName(),
+  street: faker.address.streetAddress(),
+  city: faker.address.city(),
+  state: faker.address.state(),
+  zip: faker.address.zipCode(),
+  status: Math.round(Math.random()) === 0 ? "vacant" : "occupied",
+  landlordId
+});
+
+const seedProperties = createSeedResources(createProperty, 100);
+
+module.exports = {
+  seedUsers,
+  seedProperties
+};
--- src/lib/validate-auth-input.js
+++ src/lib/validate-auth-input.js
@@ -1,3 +1,10 @@
+function isValidEmail(email) {
+  // this is a very naive non-exhausted check
+  const regex = /.+@[a-z]+\.[a-z]+/gi;
+
+  return regex.test(email);
+}
+
 function validateAuthInput(req, res, next) {
   const { email, password } = req.body;
 
@@ -5,16 +12,21 @@ function validateAuthInput(req, res, next) {
 
   if (!email) {
     errors.email = "Email is required";
+  } else if (!isValidEmail(email)) {
+    errors.email = "The email supplied is not valid";
   }
 
   if (!password) {
     errors.password = "Password is required";
+  } else if (password.length < 8) {
+    errors.password = "The password must be 8 characters long";
   }
 
   if (Object.keys(errors).length > 0) {
     res.status(400).json({
       errors
     });
+    return;
   }
 
   next();
