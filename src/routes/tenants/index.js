d90c601df64020aa2130f05e159414825c9b149d
Matt Hagner <web@matthagner.com>
2019-12-16 18:37:29 -0600

    WIP stash

---
+++ src/routes/tenants/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+// WIP 
+/* eslint-disable */
+
+// create tenant
+router.post('/', (req, res) => {
+  
+});
+
+// get all tenants
+router.get('/', (req, res) => {});
+
+// get tenant by id
+router.get('/:id', (req, res) => {});
+
+// update tenant info
+router.put('/:id', (req, res) => {});
+
+/* eslint-enable */
+
+module.exports = router;
