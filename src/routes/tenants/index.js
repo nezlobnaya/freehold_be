a950e14df84cfde8e57f7eca09958fbdf061d576
Matt Hagner <web@matthagner.com>
2019-12-16 20:37:15 -0600

    Refactor

--- src/routes/tenants/index.js
+++ src/routes/tenants/index.js
@@ -1,23 +1,3 @@
-const express = require('express');
-const router = express.Router();
+const tenantsRouter = require('./tenants');
 
-// WIP 
-/* eslint-disable */
-
-// create tenant
-router.post('/', (req, res) => {
-  
-});
-
-// get all tenants
-router.get('/', (req, res) => {});
-
-// get tenant by id
-router.get('/:id', (req, res) => {});
-
-// update tenant info
-router.put('/:id', (req, res) => {});
-
-/* eslint-enable */
-
-module.exports = router;
+module.exports = tenantsRouter;
