1c297f7c514abf6424895696b1c97ef7a40f172d
Sandy <sandy.jernigan@gmail.com>
2019-12-19 13:22:36 -0500

    refactoring some things so they can be edited with just the docs.js file.

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -1,3 +1,8 @@
+/* 
+TODO: Still refactoring some things so they can be edited in this document.
+  This will be the primary file to update the documents index page.
+*/
+
 // #region - variables for models
         
   // Users
@@ -135,31 +140,40 @@ function fromJSON(obj) {
 }
 
 const docs = {
-  changes: [
+  header: "Property Manager Backend",
+  address: "https://pt6-propman.herokuapp.com", // website address
+  changes: [ // changes should be an array of strings, these strings will become list items
     'Updated User Models, Endpoints, and Table Example',
     'Updated Property Models, Endpoints, and Table Example',
     'Updated Tenant History Models, Endpoints, and Table Example'
   ],
-  planning: [
-    '<h2>PropMan Links</h2>' +
-    'Front-End: <a href="https://propman.netlify.com/">Propman - Property Management</a> <br />' +
-    'Back-End: <a href="https://pt6-propman.herokuapp.com/">Property Manager Backend</a> <br />' +
-    '<br />' +
-    '<strong>Notion Product Board</strong>  <br />' +
-    '<a href="https://www.notion.so/Property-Manager-bd4c33b5c8974b8aa938512b993f9108">Property Manager</a> <br />' +
-    '<br />' +
-    '<strong>Trello Boards</strong> <br />' +
-    '<a href="https://trello.com/b/mKfgx81y/property-manager">Property Manager</a> <br />' +
-    '<a href="https://trello.com/b/llCWH1QF/property-manager-retrospective">Property Manager Retrospective</a> <br />' +
-    '<br />' +
-    '<strong>GitHub</strong> <br />' +
-    '<a href="https://github.com/Lambda-School-Labs/property-manager-fe">Front-End</a> <br />' +
-    '<a href="https://github.com/Lambda-School-Labs/property-manager-be">Back-End</a> <br />' +
-    '<br />' +
-    '<a href="https://www.figma.com/file/Mtjt9s7kxfGmh6fIgEszRY/PropMan">PropMan – Figma</a> <br />' +
+  planning: [ // planning should be an array of html strings, these strings will be joined with a line break between each. 
+    '<h2>PropMan Links</h2>',
+    'Front-End: <a href="https://propman.netlify.com/">Propman - Property Management</a> <br />',
+    'Back-End: <a href="https://pt6-propman.herokuapp.com/">Property Manager Backend</a> <br />',
+    '<br />',
+    '<strong>Notion Product Board</strong>  <br />',
+    '<a href="https://www.notion.so/Property-Manager-bd4c33b5c8974b8aa938512b993f9108">Property Manager</a> <br />',
+    '<br />',
+    '<strong>Trello Boards</strong> <br />',
+    '<a href="https://trello.com/b/mKfgx81y/property-manager">Property Manager</a> <br />',
+    '<a href="https://trello.com/b/llCWH1QF/property-manager-retrospective">Property Manager Retrospective</a> <br />',
+    '<br />',
+    '<strong>GitHub</strong> <br />',
+    '<a href="https://github.com/Lambda-School-Labs/property-manager-fe">Front-End</a> <br />',
+    '<a href="https://github.com/Lambda-School-Labs/property-manager-be">Back-End</a> <br />',
+    '<br />',
+    '<a href="https://www.figma.com/file/Mtjt9s7kxfGmh6fIgEszRY/PropMan">PropMan – Figma</a> <br />',
     '<a href="https://docs.google.com/document/d/1bYrgvbAEnnRviKBgIuJjOvdBhLfcCzw5stIwvk0ErM8">Labs Sprint Rubric</a> <br />'
   ],
-  address: "https://pt6-propman.herokuapp.com",
+  colorkey: [
+    '<div style="float: right;">',
+    '<span class="codesnip">Code - lightgreen</span> <br />',
+    '<span class="todo">ToDo - Yellow</span> <br />',
+    '<span class="alert">Alert - red</span> <br />',
+    '<span class="comment">Commnet - lightseagreen</span>',
+    '</div>'
+  ],
   menu: {
     planning: {
       tag: "h1",
--- src/docs/index.js
+++ src/docs/index.js
@@ -1,7 +1,7 @@
-const head = require("./head.js");
-const menu = require("./menu.js");
-const content = require("./content.js");
-const foot = require("./foot.js");
+const head = require("./lib/head.js");
+const menu = require("./lib/menu.js");
+const content = require("./lib/content.js");
+const foot = require("./lib/foot.js");
 
 module.exports = head + 
   '<div id="main"> \n' +
