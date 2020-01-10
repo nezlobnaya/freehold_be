f84179b1b1b6ad6e3ea1acf3d96f8c4ba8b8769d
Sandy <sandy.jernigan@gmail.com>
2020-01-10 01:09:28 -0500

    getById tests passed

--- src/models/workorders/workorders.test.js
+++ src/models/workorders/workorders.test.js
@@ -3,6 +3,7 @@ const Workorders = require('./workorders.js')
 
 beforeEach(async () => {
   await Db.reset()
+  await Db.seedTables()
 })
 
 afterAll(async () => {
@@ -15,21 +16,54 @@ describe('Workorder Model', () => {
 
   describe('function get', () => {
     it('Should return 2 results', async () => {
-      // call function
-      await Db.seedTables()
 
+      // call function
       const results = await Workorders.get()
+      console.log(results)
 
       // expected results
       expect(results.length).toEqual(2)
+
     })
 
     it('Should return an array', async () => {
+
       // call function
       const results = await Workorders.get()
 
       // expected results
       expect(Array.isArray(results)).toBe(true)
+
+    })
+  })
+
+  describe('function getById(id)', () => {
+    it('Should return an object', async () => {
+      
+      // Expected Input
+      const id = 1
+
+      // call function
+      const results = await Workorders.getById(id)
+      console.log(results)
+
+      // expected results
+      expect(typeof results).toBe('object')
+
+    })
+
+    it('Should return title: "Short Description" for the property with id=2', async () => {
+
+      // Expected Input
+      const id = 2
+
+      // call function
+      const results = await Workorders.getById(id)
+      console.log(results)
+      
+      // expected results
+      expect(results.title).toBe('Short Description')
+      
     })
   })
 
