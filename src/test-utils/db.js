30ffb3e378d567b9a48924ecc6972d9017ccdb3e
Sandy <sandy.jernigan@gmail.com>
2020-01-13 20:19:51 -0500

    Pulled seedData into another file

--- src/test-utils/db.js
+++ src/test-utils/db.js
@@ -1,5 +1,6 @@
-const db = require('../../database/db')
+const db = require('../../database/db.js')
 const cleaner = require('knex-cleaner')
+const seedData = require('../../database/seedData.js')
 
 const reset = async () => {
   try {
@@ -35,74 +36,12 @@ const getAllWorkorders = () => db.from('workorders as w')
 const insertWorkorders = insertResourceIntoTable('workorders as w')
 
 const seedTables = async () => {
-  // Users
-  const masterUser = {
-    firstName: 'Master',
-    lastName: 'User',
-    email: 'masterUser@gmail.com',
-    phone: '1238675309',
-    type: 'dev'
-  }
-  const landlord = {
-    firstName: 'Landlord',
-    lastName: 'User',
-    email: 'landlord@gmail.com',
-    phone: '1238675309',
-    type: 'landlord'
-  }
-  const tenant = {
-    firstName: 'Tenant',
-    lastName: 'User',
-    email: 'tenant@gmail.com',
-    phone: '1238675309',
-    type: 'tenant'
-  }
-  // Properties
-  const prop1 = {
-    'name': "Name for the Property",
-    'street': "1 First St",
-    'city': "Salt Lake City",
-    'state': "Utah",
-    'zip': "84101",
-    'status': "occupied", // vacant or occupied
-    'image': "property.jpg",
-    'landlordId': 1
-  }
-  const prop2 = {
-    'name': "Sample",
-    'street': "1 First St",
-    'city': "Salt Lake City",
-    'state': "Utah",
-    'zip': "84101",
-    'status': "vacant",
-    'landlordId': 1
-  }
-  // Work Orders
-  const workorder1 = {
-    // id: 1, (auto)
-    title: 'Work Order Title',
-    description: 'Description of the issue. For example: Tub is leaking.',
-    type: 'plumbing',
-    startDate: '10-31-2018',
-    endDate: '11-05-2018',
-    propertyId: 1,
-    createdBy: 2,
-  }
-  const workorder2 = {
-    // id: 2, (auto)
-    title: 'Short Description',
-    description: 'Description of the issue. For example: Lights not working in the living room.',
-    type: 'electrical',
-    startDate: '11-03-2019',
-    endDate: null,
-    propertyId: 1,
-    createdBy: 3
-  }
+  const { users, properties, workorders } = seedData
 
   // Insert into Tables
-  await insertUsers([landlord, tenant, masterUser])
-  await insertProperties([prop1, prop2])
-  await insertWorkorders([workorder1, workorder2])
+  await insertUsers([users.landlord, users.tenant, users.master])
+  await insertProperties([properties.prop1, properties.prop2])
+  await insertWorkorders([workorders.workorder1, workorders.workorder2])
 }
 
 module.exports = {
