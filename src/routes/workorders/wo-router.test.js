9018302c231f76006d03c7d81a303c350f3f9f8a
Sandy <sandy.jernigan@gmail.com>
2020-01-18 15:17:15 -0500

    Update wo-router.test.js

--- src/routes/workorders/wo-router.test.js
+++ src/routes/workorders/wo-router.test.js
@@ -45,6 +45,45 @@ describe('Workorder Routes', () => {
       expect(results.status).toBe(200)
       expect(results.body.length).toBe(0)
     })
+
+    it('should return a length of 2', async () => {
+      admin.verifyIdToken.mockResolvedValue({email: "tenant@gmail.com"})
+
+      const results = await request
+        .get(routeAPI)
+        .set('Authorization', 'Bearer 1234')
+
+      expect(results.body.length).toBe(2)
+    })
+  })
+
+  describe("get: '" + routeAPI + "/:id' endpoint", () => {
+    it('should return a 401 when the user is not authorized', async (id = 1) => {
+      const {error} = await request.get(routeAPI + "/" + id)
+
+      expect(error.status).toBe(401)
+    })
+
+    it('should return 200 status', async () => { 
+      admin.verifyIdToken.mockResolvedValue({email: defaultLandlord.email})
+
+      const results = await request
+        .get(routeAPI)
+        .set('Authorization', 'Bearer 1234')
+
+      expect(results.status).toBe(200)
+      expect(results.body.length).toBe(0)
+    })
+
+    it('should return a length of 2', async () => {
+      admin.verifyIdToken.mockResolvedValue({email: "tenant@gmail.com"})
+
+      const results = await request
+        .get(routeAPI)
+        .set('Authorization', 'Bearer 1234')
+
+      expect(results.body.length).toBe(2)
+    })
   })
 
 })
