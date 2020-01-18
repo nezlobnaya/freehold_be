5a7c864bbcac9dfcaecbb334763ae666e2c66213
Sandy <sandy.jernigan@gmail.com>
2020-01-18 15:17:03 -0500

    Update wo-router.js

--- src/routes/workorders/wo-router.js
+++ src/routes/workorders/wo-router.js
@@ -2,7 +2,6 @@ const express = require('express')
 const WOController = require('../../controllers/workorders/workorders.js')
 const bearerAuth = require('../../lib/bearer-auth')
 const requireAuth = require('../../lib/require-auth')
-const {requireLandlord} = require('../../middleware')
 
 const Workorders = require('../../models/workorders')
 
@@ -10,20 +9,13 @@ const router = express.Router()
 
 router.use(bearerAuth, requireAuth)
 
-const validateInput = getErrors => (req, res, next) => {
-  const errors = getErrors(req.body)
-
-  if (Object.keys(errors).length > 0) {
-    res.status(400).json({errors})
-  } else {
-    next()
-  }
-}
-
 //#region - READ
 
   // GET all workorders - based off the user who is logged in
   router.get('/', WOController.readAllByUser)
+
+  // GET workorder by id
+  router.get('/:id', WOController.readById)
 
 //#endregion
 
