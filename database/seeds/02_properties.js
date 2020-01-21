ba9138f4a28b6693f82a23654c3b9a44a1c1a125
Sandy <sandy.jernigan@gmail.com>
2020-01-21 00:35:53 -0500

    Update 02_properties.js

--- database/seeds/02_properties.js
+++ database/seeds/02_properties.js
@@ -1,28 +1,9 @@
 // const {seedProperties} = require('../../src/lib/db-helpers')
+const { properties } = require('../seedData.js')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   // return knex.from('properties').insert(seedProperties) -- mine is breaking
   
-  return knex.from("properties").insert([
-    { // id: 1, (auto)
-      'name': "Name for the Property",
-      'street': "1 First St",
-      'city': "Salt Lake City",
-      'state': "Utah",
-      'zip': "84101",
-      'status': "occupied", // vacant or occupied
-      'image': "property.jpg",
-      'landlordId': 1
-    },
-    { // id: 2, (auto)
-      'name': "Sample",
-      'street': "1 First St",
-      'city': "Salt Lake City",
-      'state': "Utah",
-      'zip': "84101",
-      'status': "vacant",
-      'landlordId': 1
-    },
-  ]);
+  return knex.from("properties").insert(properties);
 }
