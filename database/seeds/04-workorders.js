1263d282659259b6085b2dedf272b8d110585105
Sandy <sandy.jernigan@gmail.com>
2020-01-21 00:36:00 -0500

    Update 04-workorders.js

--- database/seeds/04-workorders.js
+++ database/seeds/04-workorders.js
@@ -1,25 +1,6 @@
+const { workorders } = require('../seedData.js')
+
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex.from('workorders').insert([
-    {
-      // id: 1, (auto)
-      title: 'Work Order Title',
-      description: 'Description of the issue. For example: Tub is leaking.',
-      type: 'plumbing',
-      startDate: '10-31-2018',
-      endDate: '11-05-2018',
-      propertyId: 1,
-      createdBy: 2,
-    },
-    {
-      // id: 2, (auto)
-      title: 'Short Description',
-      description: 'Description of the issue. For example: Lights not working in the living room.',
-      type: 'electrical',
-      startDate: '11-03-2019',
-      endDate: null,
-      propertyId: 1,
-      createdBy: 1
-    },
-  ])
+  return knex.from('workorders').insert(workorders)
 }
