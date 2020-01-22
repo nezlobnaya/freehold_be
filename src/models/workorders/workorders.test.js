c9cae425c17c215ea2c3cf3dc9f30a8893ab8e76
Matt Hagner <web@matthagner.com>
2020-01-21 22:36:53 -0600

    Formatting

--- src/models/workorders/workorders.test.js
+++ src/models/workorders/workorders.test.js
@@ -3,9 +3,9 @@ const Workorders = require('./workorders.js')
 
 beforeEach(async () => {
   await Db.reset()
-  await Db.seedTables("users")
-  await Db.seedTables("properties")
-  await Db.seedTables("workorders")
+  await Db.seedTables('users')
+  await Db.seedTables('properties')
+  await Db.seedTables('workorders')
 })
 
 afterAll(async () => {
@@ -15,9 +15,7 @@ afterAll(async () => {
 const defaultWorkorder = Models.createWorkorder()
 
 describe('Workorder Model', () => {
-
   describe('function add', () => {
-
     it('Should return result of an object', async () => {
       // call function
       const results = await Workorders.add(defaultWorkorder, 1, 2)
@@ -31,20 +29,20 @@ describe('Workorder Model', () => {
       const results = await Workorders.add(defaultWorkorder, 1, 2)
 
       // expected results
-      expect(results.title).toBe("New Work order")
+      expect(results.title).toBe('New Work order')
       expect(results.propertyId).toBe(1)
       expect(results.createdBy).toBe(2)
     })
 
     it('Count of all Workorders should increase by 1', async () => {
       // count before
-      const dbBefore = await Db.countResults("workorders")
-      
+      const dbBefore = await Db.countResults('workorders')
+
       // call function
       await Workorders.add(defaultWorkorder, 1, 2)
-      
+
       // count after
-      const dbAfter = await Db.countResults("workorders")
+      const dbAfter = await Db.countResults('workorders')
 
       // expected results
       expect(dbAfter - dbBefore).toEqual(1)
@@ -53,20 +51,16 @@ describe('Workorder Model', () => {
 
   describe('function get', () => {
     it('Should return an array', async () => {
-
       // call function
       const results = await Workorders.get()
 
       // expected results
       expect(Array.isArray(results)).toBe(true)
-
     })
   })
 
   describe('function getById(id)', () => {
-
     it('Should return an object', async () => {
-      
       // Expected Input
       const id = 1
 
@@ -75,54 +69,45 @@ describe('Workorder Model', () => {
 
       // expected results
       expect(typeof results).toBe('object')
-
     })
 
     it('Should return title: "Short Description" for the workorder with id=2', async () => {
-
       // Expected Input
       const id = 2
 
       // call function
       const results = await Workorders.getById(id)
-      
+
       // expected results
       expect(results.title).toBe('Short Description')
-
     })
   })
 
   describe('function getBy(query)', () => {
-
     it('Should return an Array', async () => {
-      
       // Expected Input
-      const query = { createdBy: 2 }
+      const query = {createdBy: 2}
 
       // call function
       const results = await Workorders.getBy(query)
 
       // expected results
       expect(Array.isArray(results)).toBe(true)
-
     })
 
     it('Should return 2 workorders for property with id 1', async () => {
-      
       // Expected Input
-      const query = { propertyId: 1 }
+      const query = {propertyId: 1}
 
       // call function
       const results = await Workorders.getBy(query)
-      
+
       // expected results
       expect(results.length).toEqual(2)
-
     })
   })
 
   describe('function update', () => {
-
     it('Should update title to: Updated Workorder', async () => {
       // call function
 
@@ -138,7 +123,6 @@ describe('Workorder Model', () => {
   })
 
   describe('function remove', () => {
-
     it('Should return { deleted: true } when successfully deleted', async () => {
       // call function
       const {deleted} = await Workorders.remove(2)
@@ -158,17 +142,16 @@ describe('Workorder Model', () => {
 
     it('Count of all workorders Should decrease by 1', async () => {
       // count before
-      const dbBefore = await Db.countResults("workorders")
+      const dbBefore = await Db.countResults('workorders')
 
       // call function
       await Workorders.remove(2)
-      
+
       // count after
-      const dbAfter = await Db.countResults("workorders")
+      const dbAfter = await Db.countResults('workorders')
 
       // expected results
       expect(dbBefore - dbAfter).toEqual(1)
     })
   })
-
 })
