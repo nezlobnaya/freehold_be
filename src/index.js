91ad93e4703db362c2324c6c1787e117a5744c25
nick.durbin <nick.durbin@gmail.com>
2020-04-06 21:49:07 -0400

    Made changes tot he port for the master.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const server = require('./server')
 
-const PORT = process.env.EXPRESS_PORT || 4321
+const PORT = process.env.PORT || 4321
 
 server.listen(PORT, () => {
   console.log(`🎧 Listening on port: ${PORT}\n\nBlasting off... 🚀`)
